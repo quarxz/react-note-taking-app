31bc7a2e9da179348457f6144458ab29ebb38da7
Falk König <fk@falkkoenig.de>
2024-02-22 14:38:42 +0100

    add task-list

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import { useEffect, useState } from "react";
 
 import styles from "./App.module.css";
 
+import { Task } from "./Components/Task";
+import { TaskForm } from "./Components/TaskForm";
+
+import { v4 as uuidv4 } from "uuid";
+
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: uuidv4(), todo: "Oma erschrecken" },
+    { id: uuidv4(), todo: "Katze färben" },
+    { id: uuidv4(), todo: "Klo putzen" },
+  ]);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   const [user, setUser] = useState(() => {
     const newUser = localStorage.getItem("user");
     if (newUser) {
@@ -18,6 +30,18 @@ function App() {
     email: "",
   });
 
+  function handleDeleteTask(task) {
+    setTasks(
+      tasks.filter((item) => {
+        return item.todo !== task;
+      })
+    );
+  }
+  function handleAddTask(newTask) {
+    setTasks([{ ...newTask, id: uuidv4() }, ...tasks]);
+    setIsFormOpen(false);
+  }
+
   useEffect(() => {
     function saveToLocalStorage() {
       localStorage.setItem("user", JSON.stringify(user));
@@ -31,7 +55,41 @@ function App() {
         {user.name ? <h1>Note Taking App</h1> : <h1>Login</h1>}
         {user.name ? <h2>Logged User:</h2> : undefined}
         {user.name ? <p>User: {user.name}</p> : undefined}
-        {user.name ? <p>eMial: {user.email}</p> : undefined}
+        {user.name ? <p>eMail: {user.email}</p> : undefined}
+        {user.name ? (
+          <button
+            className={styles["btn-login"]}
+            onClick={() => {
+              setIsFormOpen((prevIsFormOpen) => !prevIsFormOpen);
+            }}
+          >
+            Add ToDo
+          </button>
+        ) : undefined}
+        {isFormOpen && user.name ? (
+          <TaskForm onAddTask={handleAddTask} />
+        ) : (
+          <></>
+        )}
+        {user.name
+          ? tasks.map((task) => {
+              return (
+                <Task
+                  key={task.id}
+                  onUpdateTask={(editTask) => {
+                    setEditableToDoId(task.id);
+                    !isFormOpen
+                      ? handleUpdateTask(editTask, task.id)
+                      : undefined;
+                  }}
+                  onDeleteTask={(task) => {
+                    handleDeleteTask(task);
+                  }}
+                  todo={task.todo}
+                />
+              );
+            })
+          : undefined}
         {user.name ? undefined : (
           <form
             onSubmit={(e) => {
@@ -64,6 +122,7 @@ function App() {
 
         {user.name ? (
           <button
+            className={styles["btn-logout"]}
             onClick={() => {
               setUser({ name: "", email: "" });
             }}
